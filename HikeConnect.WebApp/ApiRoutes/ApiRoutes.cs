66bf56afe7ad413cb532a4dbe262534df73b971a
Ulquaza <mrexperio@mail.ru>
2026-04-14 16:14:26 +0700

    Create ApiRoutes.cs
    Add static class for api routes

---
+++ HikeConnect.WebApp/ApiRoutes/ApiRoutes.cs
@@ -0,0 +1,11 @@
+﻿namespace HikeConnect.WebApp.ApiRoutes
+{
+    public static class ApiRoutes
+    {
+        public static class Auth
+        {
+            public const string Login = "auth/login";
+            public const string Register = "auth/register";
+        }
+    }
+}
