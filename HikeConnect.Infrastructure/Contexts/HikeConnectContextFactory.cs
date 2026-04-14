0da0f3acb261c32743357722d6505878a4003b7d
Ulquaza <mrexperio@mail.ru>
2026-04-15 02:50:06 +0700

    Add migrations

---
+++ HikeConnect.Infrastructure/Contexts/HikeConnectContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace HikeConnect.Infrastructure.Contexts;
+
+public class HikeConnectContextFactory : IDesignTimeDbContextFactory<HikeConnectContext>
+{
+    public HikeConnectContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<HikeConnectContext>();
+
+        optionsBuilder.UseNpgsql(
+            "Host=localhost;Port=5432;Database=hikeconnect;Username=ulquaza;Password=blondesarethebest");
+
+        return new HikeConnectContext(optionsBuilder.Options);
+    }
+}
